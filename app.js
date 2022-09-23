ca9f7110c351e643695a81fbd85efab48c394032
H4V1 <javierxl20@hotmail.com>
2022-09-22 20:21:41 -0500

    update id names

--- app.js
+++ app.js
@@ -25,36 +25,47 @@ function operate(op, a, b) {
 
 const numPressed = document.querySelectorAll('.number');
 const operatorPressed = document.querySelectorAll('.operator');
-const display = document.querySelector('#result');
+const display = document.querySelector('#display');
 const equalKey = document.querySelector('.equal');
-let displayValue = 0
-let numbers = [];
-
-
-
+const clearKey = document.querySelector('.clear');
 
 let a = 0;
 let b = 0
+let result = 0;
 let operator;
+let numArr = [];
+
+// document.querySelectorAll('.btn').forEach(element => element.addEventListener('click', (e) => {
+//     display.push(e.target.textContent);
+//     result.textContent = display.join('');
+// }));
 
 numPressed.forEach(num => num.addEventListener('click', (e) => {
     let value = e.target.value;
 
     if(a > 0) {
         b = Number(value);
+        display.textContent = b;
     } else {
         a = Number(value);
+        display.textContent = a;
     }
 }));
 
 operatorPressed.forEach(op => op.addEventListener('click', (e) => {
     operator = e.target.value;
-    console.log(`${operator}`);
     
 }))
 
 equalKey.addEventListener('click', () => {
-    operate(operator, a, b)
+    display.textContent = operate(operator, a, b);
 });
+
+clearKey.addEventListener('click', () => {
+    a = 0
+    b = 0
+    numArr = [];
+    display.textContent = 0;
+})
 
 
