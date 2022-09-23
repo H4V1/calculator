933b217bb09a20f835ac16e2b01edbc9133ef817
H4V1 <javierxl20@hotmail.com>
2022-09-22 19:48:59 -0500

    use window object to call a function

--- app.js
+++ app.js
@@ -16,14 +16,45 @@ function divide(a, b) {
 }
 
 
-function operate(operator, a, b) {
-    return operator(a, b)
-}
+function operate(op, a, b) {
+    //this call a function from a string, in this case the value of a button
+    let newFunction = window[op];
+    if(typeof newFunction === 'function') newFunction(); //if the type of x = function then call the funcion x();
+    return newFunction(a, b);
+};
 
-let numPressed = document.querySelectorAll('.number');
-let displayValue = 0;
+const numPressed = document.querySelectorAll('.number');
+const operatorPressed = document.querySelectorAll('.operator');
+const display = document.querySelector('#result');
+const equalKey = document.querySelector('.equal');
+let displayValue = 0
+let numbers = [];
+
+
+
+
+let a = 0;
+let b = 0
+let operator;
 
 numPressed.forEach(num => num.addEventListener('click', (e) => {
-    displayValue = Number(document.querySelector('#result').textContent = e.target.value);
+    let value = e.target.value;
+
+    if(a > 0) {
+        b = Number(value);
+    } else {
+        a = Number(value);
+    }
+}));
+
+operatorPressed.forEach(op => op.addEventListener('click', (e) => {
+    operator = e.target.value;
+    console.log(`${operator}`);
+    
 }))
+
+equalKey.addEventListener('click', () => {
+    operate(operator, a, b)
+});
+
 
