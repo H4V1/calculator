d6825d410fa855d2f6b6ee0db25060e9e1e58705
H4V1 <javierxl20@hotmail.com>
2022-09-24 15:19:29 -0500

    calculator

--- app.js
+++ app.js
@@ -24,10 +24,83 @@ function operate(op, a, b) {
 };
 
 function resetValues() {
-    a = [];
-    b = [];
-    numArr = [];
+    numValue = 0;
+    numA = 0;
+    numB = 0;
     result = 0;
     operator = null;
 }
 
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('#display');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('#operators');
+const equalKey = document.querySelector('#equal');
+const clearKey = document.querySelector('#clear');
+
+let numValue = 0;
+let numA = 0;
+let numB = 0;
+let result = 0;
+let operator = null;
+
+buttons.forEach(num => num.addEventListener('click', (e)=> {
+    let value = e.target.value;
+    
+    if (display.textContent === '0' || display.textContent === '727 When you see it!') {
+        display.textContent = value;
+    } else {
+        display.textContent += e.target.textContent;   
+    }
+}));
+
+numbers.forEach(num => num.addEventListener('click', (e) => {
+    if(numValue === 0) {
+        numValue = e.target.value;
+    } else {
+        numValue += e.target.value
+    }
+
+    if(operator !== null) {
+        numB = Number(numValue);
+    }
+}));
+
+operators.forEach(op => op.addEventListener('click', (e) => {
+    if(numA > 0 && numB > 0) {
+        result = operate(operator, numA, numB); 
+        display.textContent = result + e.target.textContent;
+        numA = result;
+    } else if (numB === 0 && operator === 'divide') {
+        display.textContent = '727 When you see it!';
+    }
+
+    if(operator !== null) {
+        numB = Number(numValue);
+    } else {
+        numA = Number(numValue);
+    }
+    
+    operator = e.target.value;
+    numValue = 0;
+}));
+
+equalKey.addEventListener('click', () => {
+    if(numA === 0 && numB === 0) {
+        display.textContent = 0;
+        return 
+    } else if (numB === 0 && operator === 'divide') {
+        display.textContent = '727 When you see it!';
+        resetValues();
+    } else {
+        result = operate(operator, numA, numB)
+        display.textContent = result;
+        numA = result;
+        numB = 0;
+    }
+});
+
+clearKey.addEventListener('click', () => {
+    display.textContent = 0;
+    resetValues();
+});
